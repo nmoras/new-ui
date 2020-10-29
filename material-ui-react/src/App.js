b3c05d2a7f933a28eba093de8691f03bf3d0aafd
poem <normamoras@gmail.com>
2020-10-29 06:14:58 -0400

    shows copyright component

--- material-ui-react/src/App.js
+++ material-ui-react/src/App.js
@@ -1,48 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { Button, Typography, Container} from '@material-ui/core';
-import { ThemeProvider, useTheme, makeStyles, createMuiTheme } from '@material-ui/core/styles'
-import { red, green } from '@material-ui/core/colors'
+import { Container} from '@material-ui/core';
+// import { ThemeProvider, useTheme, makeStyles, createMuiTheme } from '@material-ui/core/styles'
+// import { red, green } from '@material-ui/core/colors'
 import SignIn from './components/SignIn';
 
 
-const styles = makeStyles({
-  root: {
-    backgroundColor: 'beige',
-  },
-});
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: green[500]
-    },
-    secondary:{
-      main: '#e3f2fd'
-    }
-  }
-})
-
 function App() {
-  const classes = styles(); //className={classes.root}
-  //const theme = useTheme();
+ 
 
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Container fluid className='App-header'>
-        {/* <Button 
-          variant="contained"
-          color="secondary"
-          >
-          Click me
-        </Button> */}
-        <SignIn />
-
-      </Container>
-    </ThemeProvider>
     
+    <>
+      <CssBaseline />
+      <Container maxWidth="lg" className='App-header'>
+        <SignIn />
+      </Container>  
+    </>
   );
 }
 
--- material-ui-react/src/components/SignIn.js
+++ material-ui-react/src/components/SignIn.js
@@ -26,11 +26,33 @@ const Copyright = () => {
     )
 }
 
+const useStyles = makeStyles((theme) => ({
+    paper:{
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        border: '1px solid black'
+    },
+    avatar:{
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form:{
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit:{
+        margin: theme.spacing(3, 0, 2),
+    },  
+}));
+
 function SignIn() {
     return (
-        <Container>
+        <Container component='main' maxWidth='xs'>
+            
             <Box>
-                
+                <Copyright />
             </Box>
         </Container>
     )
