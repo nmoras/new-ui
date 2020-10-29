a3091a18ca49bc0130c90cbcf94e5126db85f1f0
poem <normamoras@gmail.com>
2020-10-29 06:58:39 -0400

    shows input fields

--- material-ui-react/src/components/SignIn.js
+++ material-ui-react/src/components/SignIn.js
@@ -56,6 +56,38 @@ function SignIn() {
                 <Avatar className={classes.avatar}>
                     <LockOutlinedIcon />
                 </Avatar>
+                <Typography component='h1' variant='h5'>
+                    Sign In
+                </Typography>
+                <form className={classes.form} noValidate>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                    />
+                    <TextField 
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                    />
+                    <FormControlLabel 
+                        control={<Checkbox value="remember" color="primary" />}
+                        label="Remember me"
+                    />
+                    <Button>Sign In</Button>
+                </form>
             </div>
             <Box>
                 <Copyright />
